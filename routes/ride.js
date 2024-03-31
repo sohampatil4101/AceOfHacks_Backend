5f7c3544736eebd271b3cff85a38e4c9499271d4
Soham Patil <patilsoham390@gmail.com>
2024-03-31 10:26:48 +0530

    wallet issue solved

--- routes/ride.js
+++ routes/ride.js
@@ -165,7 +165,7 @@ router.get('/getmyname', fetchuser, async(req, res) =>{
 // get average rating 
 router.post('/getaveragerating', fetchuser, async(req, res) =>{
     try {
-        const notes = await user.find({driver: req.body.id});
+        const notes = await rating.find({driver: req.body.id});
         res.json(notes)
     } catch (error) {
     console.log(error.message)
